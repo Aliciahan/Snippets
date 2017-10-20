6b9f29290f7f8bc1d482af1d0799823a94c8f9ce
Alicia HAN <alicia.han007@gmail.com>
2017-10-20 21:25:51 +0200

    Update Rhel.sh

--- Rhel.sh
+++ Rhel.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-yum install -y automake gcc gcc-c++ kernel-devel cmake python-devel python3-devel
+yum install -y automake gcc gcc-c++ kernel-devel cmake python-devel python3-devel ncurses-devel ncurses-libs
 
 
 cd /tmp && git clone https://github.com/vim/vim.git && cd vim
-./configure --enable-pythoninterp --prefix=/usr
+./configure --enable-pythoninterp=yes --enable-python3interp=yes --prefix=/usr
 make && sudo make install
