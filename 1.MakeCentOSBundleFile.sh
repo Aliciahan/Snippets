ae8e1a791f9ea3dd7115f4d5693e206abaef17cd
Alicia HAN <alicia.han007@gmail.com>
2020-06-03 16:19:03 +0200

    commit large file

---
+++ 1.MakeCentOSBundleFile.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+docker run -v $(pwd):/root/.vim/UltiSnips --rm -it centos:8 bash  -c "\
+  yum update -y &&\
+  yum install -y vim git &&\
+  yum install -y automake gcc gcc-c++ make kernel-devel cmake python-devel python3-devel ncurses-devel ncurses-libs &&\
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim &&\
+  cp /root/.vim/UltiSnips/.vimrc /root/.vimrc &&\
+  cd /root &&\
+  vim +PluginInstall +qall &&\
+  cd /root/.vim/bundle/YouCompleteMe &&\
+  ./install.py &&\
+  cd /root/.vim &&\
+  tar -zcvf bundle-centos.tgz /root/.vim/bundle &&\
+  mv ./bundle-centos.tgz /root/.vim/UltiSnips/
+"
